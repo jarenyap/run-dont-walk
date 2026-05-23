edd6c81ebbca83eb50d71b0d79abd0d86790c9f6
pompouslupus <227606516+jarenyap@users.noreply.github.com>
2026-05-23 19:39:42 +0800

    feat: install Expo Router and set up file-based navigation structure

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Welcome to Walk Don't Run! 🏃</Text>
+            <Text>Open up app/ folder to start working on your app!</Text>
+        </View>
+    );
+}
